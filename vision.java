9a470af1d3269c0bf56aa76932629c4c55d9af06
bj54 <bj54@lab-rg07-19.cms.waikato.ac.nz>
2022-06-16 16:23:27 +1200

    working pipeline up to thresholding

--- vision.java
+++ vision.java
@@ -3,7 +3,12 @@ import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.core.Core;
+import org.opencv.core.Core.MinMaxLocResult;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+
 import org.opencv.core.Rect;
+import org.opencv.core.Size;
 
 /**
  * A simple class that demonstrates/tests the usage of the OpenCV library in
@@ -32,36 +37,82 @@ public class vision
         Mat image1 = Imgcodecs.imread(args[0]);
         Mat image2 = Imgcodecs.imread(args[1]);
 
-
 		image1 = processImg(image1);
-		
+
+		int value = matching(image1, image1);
+
 		System.out.println("Done!");
 	}
     private static Mat processImg(Mat img){
+
+        Mat grad = new Mat();
+        Mat grad_x = new Mat();
+        Mat grad_y = new Mat();
+        Mat abs_grad_x = new Mat();
+        Mat abs_grad_y = new Mat();
+        Mat dest1 = new Mat();
+        Mat dest2 = new Mat();
+        Mat thresh = new Mat();
+        Mat sharpen_2 = new Mat();
+
         // crop to roughly the eyeclass
         Rect crop = new Rect(200, 0, 1000, 1000);
-
         img = new Mat(img, crop);
         
-
+        //Convert to grayscale 
         Imgproc.cvtColor(img, img, Imgproc.COLOR_BGR2GRAY);
 
-        Imgcodecs.imwrite("cropped1.jpg", img);
+        //Chnages the contrast by equalizing histograms 
+        Imgproc.equalizeHist(img, img);
+        Imgcodecs.imwrite("contrast.jpg", img);
 
-        //Adding smoothing
-        Imgproc.medianBlur(img, img, 9);
-        //Imgproc.GaussianBlur(img, img, new Size(11, 11), 0);
+        //Adding blur to image 
+        Imgproc.GaussianBlur(img, dest1, new Size(15,15), 0);
+        Imgcodecs.imwrite("blur.jpg", dest1);
 
-        Imgcodecs.imwrite("processed.jpg", img);
-
-        //Sobel(src_gray, grad_x, ddepth, 1, 0, ksize, scale, delta, BORDER_DEFAULT);
-        //Imgproc.Canny(edges, edges, 4,12);
-        //Imgproc.Laplacian(edges, dst, CvType.CV_16S, 5, 0.6, 9, Core.BORDER_DEFAULT);
-
+        //Sharpens the image 
+        Imgproc.GaussianBlur(dest1, dest2, new Size(0,0), 10);
+        Core.addWeighted(dest1, 1.5, dest2, -0.5, 0, dest2);
+        Imgcodecs.imwrite("sharpen.jpg", dest2);
         
-        //Imgcodecs.imwrite("processed.jpg", image2);
+        //use sobal edge detection
+        Imgproc.Sobel(dest2, grad_x, CvType.CV_16SC1, 1, 0, 3, 1, 0, Core.BORDER_DEFAULT);
+        Imgproc.Sobel(dest2, grad_y, CvType.CV_16SC1, 0, 1, 3, 1, 0, Core.BORDER_DEFAULT);
+
+        Core.convertScaleAbs(grad_x, abs_grad_x);
+        Core.convertScaleAbs(grad_y, abs_grad_y);
+
+        Core.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+        Imgcodecs.imwrite("edges.jpg", grad);
+
+        //Sharpens the image 
+        Imgproc.GaussianBlur(grad, sharpen_2, new Size(0,0), 10);
+        Core.addWeighted(grad, 1.5, sharpen_2, -0.5, 0, sharpen_2);
+        Imgcodecs.imwrite("sharpen_2.jpg", sharpen_2);
+
+        //Adaptive thresholding to split the black from white 
+        Imgproc.adaptiveThreshold(sharpen_2,   thresh, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 7, 10);
+        Imgcodecs.imwrite("thresholding.jpg", thresh);
+
+        //Applies a mask to get rid of the border 
+        //Mat mask = new Mat(thresh.rows(), thresh.cols(), CvType.CV_8U, Scalar.all(0));
+
         return img;
 
+    }
+    private static int matching(Mat img, Mat templ){
+        Mat output=new Mat();
+        int matchMethod=Imgproc.TM_CCOEFF;
+        
+        
+        Imgproc.matchTemplate(img, templ, output, matchMethod);
+        
+        Imgcodecs.imwrite("match.jpg", output);
+        
+        MinMaxLocResult mmr = Core.minMaxLoc(output);
+        Point matchLoc=mmr.maxLoc;
+
+        return 1;
     }
 
 }
