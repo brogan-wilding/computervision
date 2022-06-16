063aa08f8284eae88332004ac5972f367d77e711
ge15 <ge15@lab-rg07-18.cms.waikato.ac.nz>
2022-06-16 13:33:23 +1200

    added method

--- vision.java
+++ vision.java
@@ -32,18 +32,36 @@ public class vision
         Mat image1 = Imgcodecs.imread(args[0]);
         Mat image2 = Imgcodecs.imread(args[1]);
 
-		// crop to roughly the eye
-        Rect crop = new Rect(200, 0, 1000, 1000);
-        image1 = new Mat(image1, crop);
-        image2 = new Mat(image2, crop);
 
-		Imgproc.cvtColor(image1, image1, Imgproc.COLOR_BGR2GRAY);
-        Imgproc.cvtColor(image2, image2, Imgproc.COLOR_BGR2GRAY);
-
-        Imgcodecs.imwrite("cropped1.jpg", image1);
-        Imgcodecs.imwrite("cropped2.jpg", image2);
-
+		image1 = processImg(image1);
 		
 		System.out.println("Done!");
 	}
+    private static Mat processImg(Mat img){
+        // crop to roughly the eyeclass
+        Rect crop = new Rect(200, 0, 1000, 1000);
+
+        img = new Mat(img, crop);
+        
+
+        Imgproc.cvtColor(img, img, Imgproc.COLOR_BGR2GRAY);
+
+        Imgcodecs.imwrite("cropped1.jpg", img);
+
+        //Adding smoothing
+        Imgproc.medianBlur(img, img, 9);
+        //Imgproc.GaussianBlur(img, img, new Size(11, 11), 0);
+
+        Imgcodecs.imwrite("processed.jpg", img);
+
+        //Sobel(src_gray, grad_x, ddepth, 1, 0, ksize, scale, delta, BORDER_DEFAULT);
+        //Imgproc.Canny(edges, edges, 4,12);
+        //Imgproc.Laplacian(edges, dst, CvType.CV_16S, 5, 0.6, 9, Core.BORDER_DEFAULT);
+
+        
+        //Imgcodecs.imwrite("processed.jpg", image2);
+        return img;
+
+    }
+
 }
